1e6ce89f96f8c0ac05f102fcbc6bc242cc98942a
MARIANA\MPA <mpradoa94@gmail.com>
2016-11-16 17:15:15 -0600

    Add skills and edit ocupation

--- app/src/main/java/com/example/mpa/freelancer/Profile.java
+++ app/src/main/java/com/example/mpa/freelancer/Profile.java
@@ -1,5 +1,6 @@
 package com.example.mpa.freelancer;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -7,8 +8,13 @@ import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,11 +28,13 @@ import com.parse.ParseUser;
 import org.w3c.dom.Text;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Profile extends AppCompatActivity {
 
     ImageView imageView;
     TextView name;
+    TextView occupation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,7 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         name = (TextView) findViewById(R.id.header_name);
+        occupation = (TextView) findViewById(R.id.header_occupation);
 
         //Make the profile image round
         ImageView profileImg = (ImageView)findViewById(R.id.profile_image);
@@ -50,6 +59,31 @@ public class Profile extends AppCompatActivity {
         if(currentUser != null){
             String namee = currentUser.getUsername();
             name.setText(namee);
+            Object oc = currentUser.get("ocupation");
+            if(oc != null) {
+                String ocupationn = oc.toString();
+                occupation.setText(ocupationn);
+            }
+
+            CardView layout = (CardView)findViewById(R.id.top_container);
+
+            Button newButton = new Button(this);
+            newButton.setText("Edit Profile");
+            newButton.setTextColor(0xffffffff);
+            newButton.setBackgroundColor(0xff70dc68);
+            layout.addView(newButton);
+            ViewGroup.LayoutParams params = newButton.getLayoutParams();
+            params.height = 100;
+            params.width = 300;
+            newButton.setLayoutParams(params);
+
+            newButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent = new Intent(Profile.this, EditProfile.class);
+                    startActivity(intent);
+                }
+            });
+
 ;        }
 
         /* Query test using parse
@@ -72,4 +106,6 @@ public class Profile extends AppCompatActivity {
 
 
     }
+
+
 }
---
+++ app/src/main/java/com/example/mpa/freelancer/SkillsAdapter.java
@@ -0,0 +1,59 @@
+package com.example.mpa.freelancer;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+//Adapter for skill recycleView
+public class SkillsAdapter extends RecyclerView.Adapter<SkillsAdapter.ViewHolder> {
+    private ArrayList<String> items;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string
+        public TextView textSkill;
+
+        public ViewHolder(View v) {
+            super(v);
+            textSkill = (TextView) v.findViewById(R.id.skill);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public SkillsAdapter(ArrayList<String> items) {
+        this.items = items;
+    }
+
+    @Override
+    public int getItemCount(){
+        try {
+            return items.size();
+        }catch (Exception e){
+            return 0;
+        }
+
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_skill, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textSkill.setText(items.get(position));
+
+    }
+}
