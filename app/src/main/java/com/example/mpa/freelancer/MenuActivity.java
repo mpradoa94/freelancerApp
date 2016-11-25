7fe07f4408335e67392daa7dad2ac752a7c17ab8
MARIANA\MPA <mpradoa94@gmail.com>
2016-11-25 17:48:44 -0600

    Transform search into fragment

--- app/src/main/java/com/example/mpa/freelancer/MenuActivity.java
+++ app/src/main/java/com/example/mpa/freelancer/MenuActivity.java
@@ -3,6 +3,7 @@ package com.example.mpa.freelancer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -35,7 +36,7 @@ public class MenuActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -80,14 +81,10 @@ public class MenuActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.nav_search) {
+            search searchFragment = new search();
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction().replace(R.id.contentMenu, searchFragment).commit();
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
--- app/src/main/java/com/example/mpa/freelancer/search.java
+++ app/src/main/java/com/example/mpa/freelancer/search.java
@@ -1,11 +1,21 @@
 package com.example.mpa.freelancer;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.Parse;
@@ -16,29 +26,30 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 
-public class search extends AppCompatActivity {
+public class search extends Fragment {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    ProgressDialog progressDialog;
+
     List<ParseUser> users;
-    Context context;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FragmentActivity faActivity = (FragmentActivity) super.getActivity();
+        // Replace LinearLayout by the type of the root element of the layout you're trying to load
+        RelativeLayout rLayout = (RelativeLayout) inflater.inflate(R.layout.activity_search, container, false);
+        // Of course you will want to faActivity and llLayout in the class and not this method to access them in the rest of
 
-        context = getApplicationContext();
-        mRecyclerView = (RecyclerView) findViewById(R.id.users_recycler_view);
-        final LinearLayoutManager layoutManager = new LinearLayoutManager(context);
-        //layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        //mRecyclerView.setLayoutManager(layoutManager);
-        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView = (RecyclerView) rLayout.findViewById(R.id.users_recycler_view);
+        mLayoutManager = new LinearLayoutManager(super.getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
         users = new ArrayList<>();
 
         final ParseUser currentUser = ParseUser.getCurrentUser();
+
+        progressDialog = ProgressDialog.show(super.getActivity(), "", "Loading users...", true);
 
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.whereNotEqualTo("email", currentUser.getEmail().toString());
@@ -50,6 +61,8 @@ public class search extends AppCompatActivity {
                         users.add(userObjects.get(i));
                     }
                 }
+                //Toast.makeText(getActivity(), "Users loaded", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
             }
         });
 
@@ -57,5 +70,8 @@ public class search extends AppCompatActivity {
             mAdapter = new UsersAdapter(users);
             mRecyclerView.setAdapter(mAdapter);
         }
+
+        return rLayout;
     }
+
 }
